79a52c9e9184338e564873b6e80201b345068fa6
Rebirth-star <62100185+Rebirth-star@users.noreply.github.com>
2020-09-30 12:18:51 +0400

    need npm install

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+
+import * as express from "express";
+import { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
+
+// create typeorm connection
+createConnection().then(connection => {
+    const userRepository = connection.getRepository(User);
+    debugger;
+    // create and setup express app
+    const app = express();
+    app.use(express.json());
+    app.use(express.static(__dirname));
+    // register routes
+
+    app.get("/users", async function (req: Request, res: Response) {
+        const users = await userRepository.find();
+        res.json(users);
+    });
+
+    app.get("/users/:id", async function (req: Request, res: Response) {
+        const results = await userRepository.findOne(req.params.id);
+        return res.send(results);
+    });
+
+    app.post("/users", async function (req: Request, res: Response) {
+        const user = await userRepository.create(req.body);
+        const results = await userRepository.save(user);
+        return res.send(results);
+    });
+
+    app.put("/users/:id", async function (req: Request, res: Response) {
+        const user = await userRepository.findOne(req.params.id);
+        userRepository.merge(user, req.body);
+        const results = await userRepository.save(user);
+        return res.send(results);
+    });
+
+    app.delete("/users/:id", async function (req: Request, res: Response) {
+        const results = await userRepository.delete(req.params.id);
+        return res.send(results);
+    });
+
+    // start express server
+    app.listen(8080);
+});
